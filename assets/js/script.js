6f61d26420fed1fb17e3ca456e971c2b4568bca2
mr-gantiva <ivaganva@gmail.com>
2024-07-11 17:52:04 -0400

    Agregando una alerta y validación al botón Enviar

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,15 +13,41 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 /* Tooltips */
-$(document).ready(function () {
-  /* Tooltip botón Enviar correo*/
-  $('[data-bs-toggle="tooltip"]').tooltip();
+// $(document).ready(function () {
+//   /* Tooltip botón Enviar correo*/
+//   $('[data-bs-toggle="tooltip"]').tooltip();
 
-  /* Ocultar info cards Destacados */
-  $(".card-title").on("click", function () {
-    // Seleccionar el elemento padre con la clase "card" y toggle el contenido
-    $(this).parents(".card").find(".card-text").toggle();
-    // Añadir la clase .rotate-drop-down a la imagen con id dropdown
-    $(this).find("#dropdown").toggleClass("rotate-drop-down");
+//   /* Ocultar info cards Destacados */
+//   $(".card-title").on("click", function () {
+//     // Seleccionar el elemento padre con la clase "card" y toggle el contenido
+//     $(this).parents(".card").find(".card-text").toggle();
+//     // Añadir la clase .rotate-drop-down a la imagen con id dropdown
+//     $(this).find("#dropdown").toggleClass("rotate-drop-down");
+//   });
+// });
+
+/* Validar los campos del formulario */
+document.addEventListener("DOMContentLoaded", function () {
+  const name = document.getElementById("inputNombre");
+  const asunto = document.getElementById("inputAsunto");
+  const submitButton = document.getElementById("submitBtn");
+
+  function validateForm() {
+    if (name.value.trim() !== "" && asunto.value.trim() !== "") {
+      submitButton.classList.remove("disabled");
+      submitButton.classList.add("btn-enviar");
+      submitButton.disabled = false;
+    } else {
+      submitButton.classList.add("disabled");
+      submitButton.classList.remove("btn-enviar");
+      submitButton.disabled = true;
+    }
+  }
+  name.addEventListener("input", validateForm);
+  asunto.addEventListener("input", validateForm);
+
+  $("#submitBtn").click(function () {
+    alert("Muchas gracias!\nTe estaremos contactando.");
   });
+  validateForm();
 });
